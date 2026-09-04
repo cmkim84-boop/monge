1c75fe9fe20f16a7f61db6b6c8cdb06417d4c84d
cmkim84-boop <cmkim84@ajunews.com>
2026-09-04 17:38:08 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,31 +1,8 @@
-// Import the functions you need from the SDKs you need
-
-import { initializeApp } from "firebase/app";
-
-// TODO: Add SDKs for Firebase products that you want to use
-
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-
-// Your web app's Firebase configuration
-
-const firebaseConfig = {
-
-  apiKey: "AIzaSyB3brtdwcAuUFwbh5uIyUIxn6pefk85KCo",
-
+window.firebaseConfig = {
+  apiKey: "AIzaSyB3brtdwcAuUFwbhSuIyUIxn6pefk85KCo",
   authDomain: "monge-walk.firebaseapp.com",
-
   projectId: "monge-walk",
-
   storageBucket: "monge-walk.firebasestorage.app",
-
   messagingSenderId: "719511279925",
-
-  appId: "1:719511279925:web:e702dd26d1e111b929d47c"
-
+  appId: "1:719511279925:web:e702dd26d1e111b929d47c",
 };
-
-
-// Initialize Firebase
-
-const app = initializeApp(firebaseConfig);
